a675ac36a7b1d04209068d3341692d7e5c988a22
DariWolferin <65591301+DariWolferin@users.noreply.github.com>
2021-01-22 13:52:31 +0300

    Update scriptYoga.js

--- js/scriptYoga.js
+++ js/scriptYoga.js
@@ -107,4 +107,15 @@ const itemSitemapBlock = document.querySelector('.accordeon__block');
 		})
 	}
 
+const anchors = document.querySelectorAll('a[href*="#"]');
 
+for(let anchor of anchors) {
+	anchor.addEventListener('click', function(event) {
+		event.preventDefault();
+		const blockID = anchor.getAttribute('href');
+		document.querySelector('' + blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
+	})
+}
